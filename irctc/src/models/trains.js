979ad7e49ea0f43376a66b65194d3982d45a4ac5
VIKRAM SINGH <67967129+vikky413@users.noreply.github.com>
2023-04-22 16:19:39 +0530

    Add files via upload

---
+++ irctc/src/models/trains.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
+const tSchema = new Schema({
+     name : {
+        type:String,
+        required:true,
+    },
+    number : {
+        type:String,
+        required:true,
+    }
+
+});
+
+// Compile model from schema
+const tModel = mongoose.model("tModel", tSchema);
+module.exports = tModel
